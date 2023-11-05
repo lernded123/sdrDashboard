848143d475a54db652eb6b495f4c06b9e586706c
Samuel <80083760+lernded123@users.noreply.github.com>
2023-11-05 13:12:17 -0800

    prop triggers

--- kanaban-board/src/components/AddNewTask.js
+++ kanaban-board/src/components/AddNewTask.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-const AddNewTask = () => {
-  return (
+const AddNewTask = (props) => {
+  return (props.tigger) ? (
     <div className="popup">
       <div className="popup-inner">
-        <button className="close-btn"></button>
+        <button className="close-btn">close</button>
+        {props.children}
       </div>
     </div>
   );
