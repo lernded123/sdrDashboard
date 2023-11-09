35fe51379d194e9a072fef556f65c8bb8efb5d34
Samuel <80083760+lernded123@users.noreply.github.com>
2023-11-09 13:07:26 -0800

    buttons

--- kanaban-board/src/components/AddNewTask.js
+++ kanaban-board/src/components/AddNewTask.js
@@ -3,7 +3,9 @@ import React from "react";
 const AddNewTask = () => {
   return (
     <div className="popup">
-      <div></div>
+      <div>
+        <button></button>
+      </div>
     </div>
   );
 };
