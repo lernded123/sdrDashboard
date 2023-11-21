ced9ce953c6665861b301be83d190f5a6cd6adc2
Sams <goodlifesam@gmail.com>
2023-11-21 12:40:11 -0800

    async function

--- main.js
+++ main.js
@@ -8,3 +8,21 @@ Array.from(todoItem).forEach((elem) => {
 Array.from(todoComplete).forEach((elem) => {
   elem.addEventListener("click", markIncomplete);
 });
+
+async function markComplete() {
+  const todoId = this.parentNode.dataset.todoId;
+  try {
+    const response = await fetch("todos/markComplete", {
+      method: "put",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({
+        todoIdFromJSFile: todoId,
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+    location.reload();
+  } catch (err) {
+    console.log(err);
+  }
+}
