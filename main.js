dce72b2a56b19ba0cff919dc2b22d4c6dd576c75
Sams <goodlifesam@gmail.com>
2023-11-23 12:34:06 -0800

    js

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
 const todoItem = document.querySelectorAll("span.not");
 const todoComplete = document.querySelectorAll("span.completed");
+
+Array.from(todoComplete).forEach((elem) => {
+  elem.addEventListener("click", markComplete);
+});
