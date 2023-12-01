feb3159ce36f4d1609be8021710d90ab6cafe22b
Samuel <80083760+lernded123@users.noreply.github.com>
2023-12-01 13:14:39 -0800

    popup

--- kanaban-board/src/components/AddNewTask.js
+++ kanaban-board/src/components/AddNewTask.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 const AddNewTask = (props) => {
-  return (props.tigger) ? (
+  return props.tigger ? (
     <div className="popup">
       <div className="popup-inner">
         <button className="close-btn">close</button>
         {props.children}
       </div>
     </div>
+  ) : (
+    ""
   );
 };
