f9d9368af26a628a0390e019a816935214b42e4c
Samuel <80083760+lernded123@users.noreply.github.com>
2024-01-18 11:31:34 -0800

    lots of stuff

---
+++ client/src/scenes/calendar/index.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import FullCalendar, { formatDate } from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPLugin from "@fullcalendar/interaction";
+import listPlugin from "@fullcalendar/list";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import Header from "../../componetns/Header";
+import { tokens } from "../../theme";
+
+const Calendar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [currentEvenes, setCurrentEvenes] = useState([]);
+
+  const handleDateclick = (selected) => {
+    const title = prompt("Please enter a new title for your event");
+    const calendarApi = selected.view.calendar;
+    calendarApi.unselect();
+
+    if (title) {
+      calendarApi.addEvent({
+        id: `${selected.dateStr}-${title}`,
+        title,
+        start: selected.startStr,
+        end: selected.endStr,
+        allday: selected.allday,
+      });
+    }
+  };
+
+  const handleEventclick = (selected) => {
+    if (
+      window.confirm(
+        `Are you sure you want to delete the event '${selected.event.title}'`
+      )
+    ) {
+      selected.event.remove();
+    }
+  };
+
+  return (
+    <Box m="20px">
+      <Header title="CALENDAR" subtitle="FullCalendar Interactive Page" />
+      <Box></Box>
+    </Box>
+  );
+};
+
+export default Calendar;
---
+++ client/src/scenes/form/index.jsx
@@ -0,0 +1,21 @@
+//User validation so wont need this
+
+import { Box, Button, TextField } from "@mui/material";
+import { Formik } from "formik";
+import * as yup from "yup";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import Header from "../../components/Header";
+
+const Form = () => {
+  const isNonMobile = useMediaQuery("(min-width:600px)");
+
+  const handleFormSubmit = (values) => {
+    console.log(values);
+  };
+
+  //   return <Box m="20px">
+  //     <Header title="CREATE USER"
+  //   </Box>;
+};
+
+export default Form;
