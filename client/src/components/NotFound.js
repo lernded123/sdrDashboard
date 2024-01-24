e952cf6296a3e263a6869b8f3c6a414c0f18b7aa
Samuel <80083760+lernded123@users.noreply.github.com>
2024-01-24 13:31:45 -0800

    not found page

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -3,9 +3,13 @@ import React from "react";
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>404 - Not Found</h1>
-      <p>Sorry, the page you are looking for does not exist.</p>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1 style={{ fontSize: "3em" }}>404 - Page Not Found</h1>
+      <p style={{ fontSize: "1.5em" }}>
+        Looks like you've followed a broken link or entered a URL that doesn't
+        exist on this site.
+      </p>
+      {/* Add any additional content or links as needed */}
     </div>
   );
 };
