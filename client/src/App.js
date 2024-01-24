5511a18b705285a88fd58c3b46669f7fbc50a1d4
Samuel <80083760+lernded123@users.noreply.github.com>
2024-01-24 13:29:59 -0800

    not found added

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
 //import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
+import NotFound from "./components/NotFound";
 //import Bar from "./scenes/bar";
 // import Form from "./scenes/form";
 // import Line from "./scenes/line";
@@ -30,6 +31,7 @@ function App() {
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
+              <Route path="*" element={<NotFound />} />
               {/*<Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
               <Route path="/bar" element={<Bar />} />
